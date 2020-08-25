a1c5232c95181864c221f33b921432930ac186e0
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-08-25 17:59:32 +0200

    Add files via upload

---
+++ Internship/Code/IntraclonalDiversity/clonotype.php
@@ -0,0 +1,11 @@
+<?php
+  require_once('class/tools.php');
+
+  $userFile = $_COOKIE['Userfile'];
+  echo "<!DOCTYPE html><html>";
+  echo debut_html("Intraclonal visualization - clonotype");
+  echo "<body>";
+  include('includes/navbar.html');
+  include('includes/clonotype.html');
+  echo "</body></html>";
+?>
