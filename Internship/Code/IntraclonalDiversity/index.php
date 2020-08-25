a75759b98214ec7a086bd1c68556adc272a29a19
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-08-25 08:29:36 +0200

    Create index.php

---
+++ Internship/Code/IntraclonalDiversity/index.php
@@ -0,0 +1,11 @@
+<?php
+  require_once('class/tools.php');
+
+  echo "<!DOCTYPE html><html>";
+  echo debut_html("Intraclonal visualization");
+  echo "<body>";
+  include('includes/navbar.html');
+  include('includes/form.html');
+  echo "</body></html>";
+
+?>
