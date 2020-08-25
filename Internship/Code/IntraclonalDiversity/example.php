2436746e68cc01704c3a5d7a38ab0cd0ce17121b
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-08-25 17:58:34 +0200

    Update example.php

--- Internship/Code/IntraclonalDiversity/example.php
+++ Internship/Code/IntraclonalDiversity/example.php
@@ -5,10 +5,9 @@ ini_set("display_errors", 1);
   require_once('class/tools.php');
   setcookie('Userfile', 'example', time() + 365*24*3600, null, null, false, true); 
   echo "<!DOCTYPE html><html>";
-  echo debut_html("Intraclonal visualization - Repertoire");
+  echo debut_html("Intraclonal visualization - Clone");
   echo "<body>";
+  include('includes/navbar.html');
   include('includes/clone.html');
   echo "</body></html>";
-
-
 ?>
