4377dc3e923e56aaa44ee915c62e8580b532bc5d
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-08-25 08:32:26 +0200

    Create tools.php

---
+++ Internship/Code/IntraclonalDiversity/class/tools.php
@@ -0,0 +1,17 @@
+<?php
+
+define('ROOT_FOLDER', '/IntraclonalDiversity');
+
+function debut_html($title){
+  return 
+  "<head>
+   <title> $title </title>
+
+   <link rel='stylesheet' href='" . ROOT_FOLDER . "/css/style.css'>   
+
+   <meta http-equiv='Content-Type' content='text/html;charset=utf-8' />\n
+   <script src='https://d3js.org/d3.v4.min.js' charset='utf-8'></script>\n
+   </head>";
+}
+
+?>
