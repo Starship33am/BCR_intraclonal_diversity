c53ba8a6001bd5a884d8cf3ea32d97546bbc03d7
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-09-24 10:32:23 +0200

    Add files via upload

--- Internship/Code/IntraclonalDiversity/class/tools.php
+++ Internship/Code/IntraclonalDiversity/class/tools.php
@@ -7,8 +7,8 @@ function debut_html($title){
   "<head>
    <title> $title </title>
 
-   <link rel='stylesheet' href='" . ROOT_FOLDER . "/css/style.css'>   
-   <link rel='stylesheet' href='" . ROOT_FOLDER . "/css/repertoire.css'>   
+   <link rel='stylesheet' href='" . ROOT_FOLDER . "/css/style.css'> 
+   <link rel='stylesheet' href='" . ROOT_FOLDER . "/css/repertoire.css'>    
 
    <meta http-equiv='Content-Type' content='text/html;charset=utf-8' />\n
    <script src='https://d3js.org/d3.v4.min.js' charset='utf-8'></script>\n
