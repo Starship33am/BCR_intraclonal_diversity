759fc69e8ea085d1554b8e2010dd7abb9fe008ee
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-12-07 12:59:52 +0100

    Update verification.php

--- Internship/Code/IntraclonalDiversity/verification.php
+++ Internship/Code/IntraclonalDiversity/verification.php
@@ -3,52 +3,53 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-  require_once('class/tools.php');
   $userFile = (string) rand(0, 1000000);
-  $oldMask = umask(0);
-  mkdir("pipeline/usersFiles/" . $userFile, 0777,true);
-  umask($oldMask);
-
-  //setcookie("Userfile", $userFile, "");
-  setcookie('Userfile', $userFile, time() + 365*24*3600, null, null, false, true); 
-
-  echo "<!DOCTYPE html><html>";
-  echo debut_html("Visualizing intraClonal Diversity");
-  echo "<body>";
 
   if(isset($_POST['submit'])){
-    $storeFolder = "pipeline/usersFiles/" . $userFile;
     //count total files
     $countFiles = count(array_filter($_FILES['file']['name']));
-    if($countFiles==3){
-      $files = array(); //contain the path where the files will be store
-      for($i=0;$i<$countFiles;$i++){
-        $fileName = $_FILES['file']['name'][$i]; 
-        $files[$i] = $storeFolder . "/" . $fileName;
-        move_uploaded_file($_FILES['file']['tmp_name'][$i], $storeFolder . '/' . $fileName);
-      }
-      // format error
-      $error = checkFastaFile($files[0]);
-      if($error==0){
+    $analysisTitle = $_POST['title'];
+    //the user has given a title to the analysis
+    if(!empty($analysisTitle)){
+      //there is 3 files
+      if($countFiles==3){
+        $oldMask = umask(0);
+        mkdir("pipeline/usersFiles/" . $userFile, 0777,true);
+        umask($oldMask);
+        $storeFolder = "pipeline/usersFiles/" . $userFile;
+        $files = array(); //contain the path where the files will be store
+        for($i=0;$i<$countFiles;$i++){
+          $fileName = $_FILES['file']['name'][$i]; 
+          $files[$i] = $storeFolder . "/" . $fileName;
+          move_uploaded_file($_FILES['file']['tmp_name'][$i], $storeFolder . '/' . $fileName);
+        }
+        // format error
+        $error = checkFastaFile($files[0]);
+        if($error=="fasta"){
+          //COOKIES
+          setcookie('Userfile', $userFile, time() + 365*24*3600, null, null, false, true);
+          setcookie('title', $analysisTitle, time() + 365*24*3600, null, null, false, true); 
+
           header('Location: gtm.php');
           exit();
+        }else{
+          echo '<script type="text/javascript">window.alert("'. $error .'");</script>';
+        }
       }else{
-        echo "<p class='error' >", $error, "</p>";
+      echo '<script type="text/javascript">window.alert("ERROR:: You should provide tree files");</script>';
       }
     }else{
-      echo "<p class='error' >", "ERROR:: You should provide tree files", "</p>";
+      echo '<script type="text/javascript">window.alert("ERROR:: You should provide a name to your analysis");</script>';
     }
   }
-
-  echo "</body></html>";
-
 
   function checkFastaFile($file){
     $pathParts = pathinfo($file);
     $fileName = $pathParts['basename'];
     $fileHandle = fopen($file, "r");
-    $seq = ""; 
-    while(! feof($fileHandle)){
+    $header = false;
+    $seq = "";
+    while(!feof($fileHandle)){
       $line = fgets($fileHandle);
       if ($line[0]=='>'){
         $header = true;
@@ -62,27 +63,7 @@ ini_set("display_errors", 1);
       }
     }
     fclose($fileHandle);
-    return 0;
+    return "fasta";
   }
-/*
-  function checkIMGTFormat($files){}
-
-  function IMGTSummary($file){
-    $pathParts = pathinfo($file);
-    $fileName = $pathParts['basename'];
-    $fileHandle = fopen($file, "r");
-    $header = "Sequence number	Sequence ID	V-DOMAIN Functionality	V-GENE and allele	V-REGION score	V-REGION identity %	V-REGION identity nt	V-REGION identity % (with ins/del events)	V-REGION identity nt (with ins/del events)	J-GENE and allele	J-REGION score	J-REGION identity %	J-REGION identity nt	D-GENE and allele	D-REGION reading frame	CDR1-IMGT length	CDR2-IMGT length	CDR3-IMGT length	CDR-IMGT lengths	FR-IMGT lengths	AA JUNCTION	JUNCTION frame	Orientation	V-DOMAIN Functionality comment	V-REGION potential ins/del	J-GENE and allele comment	V-REGION insertions	V-REGION deletions	Sequence	5prime trimmed-n nb	3prime trimmed-n nb	Analysed sequence length	Sequence analysis category	";
-    $line=fgets($fileHandle);
-    if($line==header){
-      $line=fgets($fileHandle);
-      
-    }
-  }
-
-  function IMGTGap($file){
-    Sequence number	Sequence ID	V-DOMAIN Functionality	V-GENE and allele	J-GENE and allele	D-GENE and allele	V-D-J-REGION	V-J-REGION	V-REGION	FR1-IMGT	CDR1-IMGT	FR2-IMGT	CDR2-IMGT	FR3-IMGT	CDR3-IMGT	JUNCTION	J-REGION	FR4-IMGT	
-
-  }*/
-
 
 ?>
