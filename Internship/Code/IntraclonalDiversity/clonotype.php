506e6014e0b0a785c7018c3197cf01de10b36bbe
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2020-12-07 12:55:32 +0100

    Update clonotype.php

--- Internship/Code/IntraclonalDiversity/clonotype.php
+++ Internship/Code/IntraclonalDiversity/clonotype.php
@@ -2,6 +2,7 @@
   require_once('class/tools.php');
 
   $userFile = $_COOKIE['Userfile'];
+  $title = $_COOKIE["title"];
   echo "<!DOCTYPE html><html>";
   echo debut_html("Intraclonal visualization - clonotype");
   echo "<body>";
@@ -9,4 +10,3 @@
   include('includes/clonotype.html');
   echo "</body></html>";
 ?>
-
