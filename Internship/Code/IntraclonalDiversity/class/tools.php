71af406fce508408164a593d1eda053039511182
LucileJeusset <62330021+LucileJeusset@users.noreply.github.com>
2021-02-03 10:10:07 +0100

    Update tools.php

--- Internship/Code/IntraclonalDiversity/class/tools.php
+++ Internship/Code/IntraclonalDiversity/class/tools.php
@@ -3,6 +3,7 @@
 define('ROOT_FOLDER', '/IntraclonalDiversity');
 
 function debut_html($title){
+
   return 
   "<head>
    <title> $title </title>
@@ -12,7 +13,10 @@ function debut_html($title){
 
    <meta http-equiv='Content-Type' content='text/html;charset=utf-8' />\n
    <script src='https://d3js.org/d3.v4.min.js' charset='utf-8'></script>\n
+   <script src='https://cdnjs.cloudflare.com/ajax/libs/jszip/3.5.0/jszip.min.js'></script>\n
+   <script src='http://cdn.jsdelivr.net/g/filesaver.js'></script>\n
    </head>";
 }
+
 
 ?>
