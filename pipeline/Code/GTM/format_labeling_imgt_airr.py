e7dd0c353a2c176c47c0832d56a465324acdee9f
Nika Abdollahi <nikaabdollahi@gmail.Com>
2021-06-10 16:33:40 +0200

    add abundance of unique sequences

--- pipeline/Code/GTM/format_labeling_imgt_airr.py
+++ pipeline/Code/GTM/format_labeling_imgt_airr.py
@@ -11,11 +11,24 @@ def read_output_file(filename):
 	f.close()
 	return lines
 
+
 #####################################################################
-def dico_V_J_CDR3_format(AIRR):
+def filter_dico(Dico,remaine_seq) :
+	print ("avant",len(Dico))
+	list_all_seq = Dico.keys()
+	to_delet = set(list_all_seq) - set(remaine_seq)
+
+	for seq in to_delet:
+		del Dico[seq]
+	print ("apres",len(Dico))
+	return Dico, len(to_delet)
+
+#####################################################################
+def dico_V_J_CDR3_format(AIRR,abundance_threshold):
 	lines = read_output_file(AIRR)
-	low_quality_seq=[]
+	low_quality_seq, remaine_seq =[], []
 	Dico={}
+	Dico_uniq_adundance = {}
 	for l in range(1,len(lines)):
 		split=lines[l].split("\t")
 		#split[13] is the sequence alignment, is better than the sequence because it starts at the beginig of the V and ends at the end of J 
@@ -40,17 +53,29 @@ def dico_V_J_CDR3_format(AIRR):
 				Jseq = split[89]
 				#print (CDR3)
 			Dico[sequence_id] = [functionality,V,J,CDR3,Jseq]
+			# keep the sequences based on the read abundace
+			if split[13] in Dico_uniq_adundance.keys() :
+				Dico_uniq_adundance[split[13]].append(sequence_id)
+			else : 
+				Dico_uniq_adundance[split[13]] = [sequence_id]
+	for key in Dico_uniq_adundance.keys():
+		if len(Dico_uniq_adundance[key]) > abundance_threshold : 
+			remaine_seq.append(Dico_uniq_adundance[key])
+	flat_list_remaine_seq = [item for sublist in remaine_seq for item in sublist]
+	#delete low abundant unique sequences 
+	final_dico, deleted = filter_dico(Dico,flat_list_remaine_seq)
+	number_of_analyzed_seq = len(Dico.keys()) - deleted
 	print("Total sequence count : " , len(lines)-1)
 	print("Number of low quality sequences that have been eliminated from the analysis : ",  len(low_quality_seq))
-	print("Number of sequences to be analysed : ",  (len(lines)-1)-len(low_quality_seq))
-	return Dico
+	print("Number of low abundant sequences that have been eliminated from the analysis, using the threshold of "+ str(abundance_threshold)+": ",  deleted)
+	print("Number of sequences to be analysed : ",  (len(lines)-1)-len(low_quality_seq)-deleted)
+	return final_dico
 
 #####################################################################				
 def write_file(Dico_VJCDR3,output_file):
 	outputname = output_file.split(".")[0]+"_V_CDR3_Jseq.txt"
 	f = open(outputname,"w")
 	for key in Dico_VJCDR3.keys():
-		#print (Dico_VJCDR3[key])
 		line = key + "\t" + Dico_VJCDR3[key][0] + "\t" + Dico_VJCDR3[key][1] + "\t" + Dico_VJCDR3[key][2] + "\t" + Dico_VJCDR3[key][3] + "\t" + Dico_VJCDR3[key][4] + "\n"
 		f.write(line)
 	f.close()
@@ -60,20 +85,23 @@ def write_file(Dico_VJCDR3,output_file):
 ####################################################################
 def main():
 	start_time = time.time()
-	usage = "usage: format_labeling_imgt.py -a AIRR -o output_file"
+	usage = "usage: format_labeling_imgt.py -a AIRR -o output_file -t abundance_threshold"
 	parser = OptionParser(usage)
 	parser.add_option("-a", "--AIRR", dest="AIRR",
 	      help="read data from AIRR")
 	parser.add_option("-o", "--output_file",dest="output_file",
 	      help="write data to output_file")
+	parser.add_option("-t", "--abundance_threshold",dest="abundance_threshold",
+	      help="the abundance threshold of unique sequences to be analyzed, default = 3")
 	(options, args) = parser.parse_args()
-	if len(sys.argv) != 5:
+	if len(sys.argv) != 7:
 		parser.error("incorrect number of arguments")
 	
 	AIRR_file = options.AIRR
 	output_file = options.output_file
+	abundance_threshold = options.abundance_threshold
 	time_start = time.perf_counter()
-	Dico_VJCDR3 = dico_V_J_CDR3_format(AIRR_file)
+	Dico_VJCDR3 = dico_V_J_CDR3_format(AIRR_file,int(abundance_threshold))
 	write_file(Dico_VJCDR3,output_file)
 	print("The AIRR file reading step execution time : %s seconds " % (time.time() - start_time))
 
